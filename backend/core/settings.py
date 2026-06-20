58d2c4c0509aff82166dd8d64ee9be595b853c49
ale1010111 <alejandro71133089mail.com>
2026-06-20 13:10:52 -0400

    fix: add EMAIL_USE_SSL and EMAIL_TIMEOUT to settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -252,12 +252,13 @@ CSRF_TRUSTED_ORIGINS   = env("CSRF_TRUSTED_ORIGINS")
 
 EMAIL_BACKEND       = env("EMAIL_BACKEND",       default="django.core.mail.backends.console.EmailBackend")
 EMAIL_HOST          = env("EMAIL_HOST",          default="smtp.gmail.com")
-EMAIL_PORT          = env("EMAIL_PORT")
-EMAIL_USE_TLS       = env("EMAIL_USE_TLS")
+EMAIL_PORT          = env.int("EMAIL_PORT",      default=587)
+EMAIL_USE_TLS       = env.bool("EMAIL_USE_TLS",  default=True)
+EMAIL_USE_SSL       = env.bool("EMAIL_USE_SSL",  default=False)
+EMAIL_TIMEOUT       = env.int("EMAIL_TIMEOUT",   default=10)
 EMAIL_HOST_USER     = env("EMAIL_HOST_USER",     default="")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL  = env("DEFAULT_FROM_EMAIL",  default="ANH <noreply@anh.gob.bo>")
-
 # ------------------------------------------------
 # FRONTEND URL
 # Usada para construir enlaces en emails
